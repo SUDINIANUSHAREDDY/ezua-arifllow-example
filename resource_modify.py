517745a710b5c7f4fd7a697c4f56dd148dc72bf6
anusha_s <37624944+SUDINIANUSHAREDDY@users.noreply.github.com>
2025-07-28 22:47:50 -0700

    Update resource_modify.py

--- resource_modify.py
+++ resource_modify.py
@@ -7,22 +7,27 @@ default_args = {
 }
 
 with DAG(
-    'example_kubernetes_pod_operator_xcom_resources_dict',
+    'example_kubernetes_pod_operator_xcom_resources_dict_with_annotations',
     schedule_interval=None,
     default_args=default_args,
     catchup=False,
     tags=['example'],
 ) as dag:
 
+    pod_annotations = {
+        "dag_id": "{{ dag.dag_id }}",
+        "run_id": "{{ run_id }}",
+    }
+
     resources = {
-        "cpus": 1,    # CPU cores as integer
-        "ram": 512,   # RAM in MB
-        "disk": 0,    # Disk in MB (0 if not used)
-        "gpus": 0     # Number of GPUs
+        "cpus": 1,
+        "ram": 512,
+        "disk": 0,
+        "gpus": 0,
     }
 
     kpo_task = KubernetesPodOperator(
-        namespace='default',
+        namespace='admin-635a7131',  # <-- updated namespace here
         image='python:3.9-slim',
         cmds=["python", "-c"],
         arguments=[
@@ -36,6 +41,7 @@ with DAG(
         do_xcom_push=True,
         resources=resources,
         is_delete_operator_pod=True,
+        pod_annotations=pod_annotations,
     )
 
     kpo_task
